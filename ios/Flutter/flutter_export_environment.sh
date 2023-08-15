fe6ebc6cd0aa455e98f61f35ae4db7cbd971e194
AlexSoul1903 <alexmanuel19frias@gmail.com>
2023-08-14 22:28:28 -0400

    Added Logo to apk

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter_windows_3.10.3-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\alexm\OneDrive\Documentos\6to cuatrimestre de clases itla tareas\Programacion Movil\TripTrekker"
+export "FLUTTER_APPLICATION_PATH=C:\Users\alexm\OneDrive\Documentos\6to cuatrimestre de clases itla tareas\Programacion Movil\TripTrekker ver3\TripTrekker"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
